18eee7afbde2bc691a0567119f96274c5b76f002
Fer-efe <ferpolanco83@gmail.com>
2024-05-06 09:45:36 -0300

    Ingresando cambios en comentarios de ejercicio

--- claveEureka.ts
+++ claveEureka.ts
@@ -1,30 +1,29 @@
 import * as readlineSync from "readline-sync";
 
-// Definir la clave correcta
+// Clave correcta
 const claveCorrecta: string = "eureka";
 
-// Inicializar el contador de intentos fallidos
+// Contador de intentos erróneos
 let intentosFallidos: number = 0;
 
-// Repetir hasta que se acierte la clave o se agoten los intentos
 while (intentosFallidos < 3) {
-    // Pedir al usuario que ingrese la clave
+    // Solicitar al usuario el ingreso de clave
     const claveIngresada: string = readlineSync.question("Ingrese la clave: ");
 
-    // Verificar si la clave ingresada es correcta
+    // Verificacion de clave ingresada
     if (claveIngresada === claveCorrecta) {
-        // Mostrar mensaje de éxito y salir del programa
+        // Mensaje de clave correcta y salir del programa
         console.log("¡Clave correcta! Has accedido al sistema.");
         break;
     } else {
-        // Incrementar el contador de intentos fallidos
+        // Incremento de contador de intentos erroneos
         intentosFallidos++;
 
-        // Mostrar mensaje de intento fallido y el número de intentos restantes
+        // Mensaje de intento erroneo y el número de intentos
         const intentosRestantes: number = 3 - intentosFallidos;
         console.log(`Clave incorrecta. Te quedan ${intentosRestantes} intentos.`);
 
-        // Si se agotan los intentos, mostrar mensaje y salir del programa
+        // Agotados los intentos mostrar mensaje y salir del programa
         if (intentosFallidos === 3) {
             console.log("Has agotado todas las oportunidades. Inténtalo de nuevo más tarde.");
         }
