6c242c63f39acca8dd588adeca46cadd6ece6026
Fer-efe <ferpolanco83@gmail.com>
2024-05-30 14:39:30 -0300

    agregando ejercicio Arreglo de Numeros (Posiciones)

---
+++ arregloDeNumerosPosiciones.js
@@ -0,0 +1,23 @@
+// declaro array
+var numb = [20, 14, 8, 0, 5, 19, 24];
+//creo variable apara indice 0
+var indice = 0;
+//Si el indice es mayor a 7 muestro por pantalla el resultado
+while (indice < 7) {
+    console.log("El numero en la posición ", indice, "es", numb[indice]);
+}
+/*
+let num2 : number[] = new Array (7) ;
+let indice: number = 0;
+num[0] = 20;
+num[1] = 14;
+num[2] = 8;
+num[3] = 0;
+num[4] = 5;
+num[5] = 19;
+num[6] = 24;
+while (indice < 7) {
+console.log ("El numero en la posicion ", indice, " es ", num2[indice]);
+indice++;
+}
+*/ 
