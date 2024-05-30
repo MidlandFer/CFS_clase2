cf4d07c561968488bca9db46f8b5675be8fac4fa
MidlanFer <ferpolanco83@gmail.com>
2024-05-30 19:48:11 -0300

    agrego comentarios

--- dosArreglos.ts
+++ dosArreglos.ts
@@ -1,17 +1,21 @@
 import * as readlineSync from "readline-sync";
-
+//se crean los arreglos una para number y otro para string
 let arregloNombres : string[] = new Array (2);
 let arregloNumeros : number[] = new Array (3);
+// se crea la variable indice para recorrer el arreglo con for, agregando el ingreso por teclado de string
 let indice : number;
 for (indice = 0; indice < 2; indice++) {
 arregloNombres[indice] = readlineSync.question(`Ingrese el nombre de la posicion ${indice}: `);
 }
+//Se recorre el arreglo, agregando el ingreso por teclado del usuario number
 for (indice = 0; indice < 3; indice++) {
 arregloNumeros[indice] = readlineSync.questionInt(`Ingrese el numero de la posicion ${indice}: `);
 }
+//se muestra por consola los datos ingresados por teclado del usuario string
 for (indice = 0; indice < 2; indice++) {
 console.log(`El nombre en la posicion ${indice} es: ${arregloNombres[indice]}`);
 }
+//se muestra por consola los datos ingresados por teclado del usuario number
 for (indice = 0; indice < 3; indice++) {
 console.log(`El numero en la posicion ${indice} es: ${arregloNumeros[indice]}`);
 }
