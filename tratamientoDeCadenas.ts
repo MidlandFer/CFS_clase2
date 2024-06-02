513d0b4bb15959933da0affec3ebca5926bb971a
MidlanFer <ferpolanco83@gmail.com>
2024-06-02 14:15:49 -0300

    ejercicio tratamiento de cadenas

---
+++ tratamientoDeCadenas.ts
@@ -0,0 +1,7 @@
+import * as rls from "readline-sync";
+
+
+let nombreApellido : string = "Juan Perez";
+let inicialNombre : string = nombreApellido[0];
+let inicialApellido : string = nombreApellido[5];
+console.log("Nombre y Apellido: ‘,nombreApellido,’ Iniciales: ‘,inicialNombre,inicialApellido");
