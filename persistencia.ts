cf159a8517d98d36e55211706b5bbd26a1992989
MidlanFer <ferpolanco83@gmail.com>
2024-06-07 20:58:32 -0300

    guardando ejercicio de clase 11

--- persistencia.ts
+++ persistencia.ts
@@ -17,4 +17,10 @@ for(let i:number=0;i< nombres.length; i++){
 fs.writeFileSync('./test.txt', contenido);
 console.log("Finalizado");
 
+const datos:string= fs.readFileSync('./test.txt','utf8');
+const datos2:string=datos.trim();
+const nuevoArray:string[]=datos2.split(" ");
+console.log(nuevoArray);
+
+
 //  ''
