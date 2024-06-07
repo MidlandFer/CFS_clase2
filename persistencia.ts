26ec4249c8ed2807216f17e66c1500e5bbe3f9dd
MidlanFer <ferpolanco83@gmail.com>
2024-06-07 19:50:11 -0300

    realizo modificaiones

--- persistencia.ts
+++ persistencia.ts
@@ -1,4 +1,4 @@
-//import fs from 'node:fs';
+import fs from 'node:fs';
 
 //let tercerContenido:string="tercerContenido";
 //let otroContenido:string="otorContenido";
@@ -11,10 +11,10 @@ const nombres:string[]=["juan", "Analia" ,"Nicolas" , "Carla"];
 let contenido:string="";
 
 for(let i:number=0;i< nombres.length; i++){
-    contenido += '${nombres[i]}'
+    contenido += ' ${nombres[i]} '  //template string ---> nombre[i] + "";
 }
 
-fs.writeFileSync('./test.txt, contenido, otroContenido, tercerContenido');
+fs.writeFileSync('./test.txt', contenido);
 console.log("Finalizado");
 
 //  ''
