1a265ec6539e88e993c2b078de19f09954068db9
MidlanFer <ferpolanco83@gmail.com>
2024-06-10 20:08:09 -0300

    ejercicio

--- persistenciaObligatorio.ts
+++ persistenciaObligatorio.ts
@@ -1,29 +1,16 @@
 import * as fs from 'node:fs';
- const costo:number[]=[3000, 3400, 3500, 3300];
- const cigarros:string[]=["Marlboro" , "Parliament" , "Camel" , "Cheserfield"];
+ 
+const cigarros:string[]=["Marlboro" , "Parliament" , "Camel" , "Chesterfield"];
+let precio:string= "  ";
 
- //let costo:string=costo.toString();
-let costoCigarros:string="";
-let productoCigarros:string="";
-
-for(let i:number=0;i< costoCigarros.length; i++){
-    costoCigarros+= `${costoCigarros[i]}  `;
-    productoCigarros+= `${productoCigarros[i]}  `;
-
+for(let i:number=0;i< cigarros.length; i++){
+    precio+= `${cigarros[i]}  `;
+   
     }
-    fs.writeFileSync('./costo.txt', costoCigarros);
-    fs.writeFileSync('./cigarros,.txt' , productoCigarros);
-    console.log("costo");
-    console.log("producto");
-
- const infoPrecios:string =fs.readfileSync('./costo.txt' , 'utf8 ');
- const infoProductos:string =fs.readfileSync('./cirgarros.txt ' , 'utf8 ');
- const info1:string =infoPrecios.trim();
- const info2:string=infoPrecios.trim();
-
- let array1:string[]=info1.split(" ");
- let array2:string[]=info2.split(" ");
+    fs.writeFileSync('./cigarros.txt', precio);
+    console.log("Terminado");
+   
+ const datos:string =fs.readFileSync('./cigarros.txt ' , 'utf8');
+ const datos2:string =datos.trim();
+ const array1:string[]=datos2.split(" ");
  console.log(array1);
- console.group(array2);
-
- )
